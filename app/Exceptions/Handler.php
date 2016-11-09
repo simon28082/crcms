23fb8f05338db7b5a83b0e88a956e7c6f0de55bf
hiword <28737164@qq.com>
2016-11-09 13:59:23 +0800

    增加路由选项 Handler debug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Exceptions;
-
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
 //        App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
 //        App\Providers\EventServiceProvider::class,
-//        App\Providers\RouteServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
 
         \CrCms\Kernel\Providers\KernelServiceProvider::class,
         \CrCms\Manage\Providers\ManageServiceProvider::class,
