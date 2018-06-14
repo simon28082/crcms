6a6e8518750a99af580fd309dcc29ef08e81631d
crcms <crcms@crcms.cn>
2018-06-15 07:37:34 +0800

    application update

--- public/index.php
+++ public/index.php
@@ -25,22 +25,6 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
-
-$app = new \CrCms\Foundation\Application(
-    realpath(__DIR__.'/../')
-);
-
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -53,7 +37,7 @@ $app = new \CrCms\Foundation\Application(
 |
 */
 
-require_once '../vendor/crcms/framework/src/Bootstrap/app.php';
+$app = require_once __DIR__ . '/../vendor/crcms/framework/src/Bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
