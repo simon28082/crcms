e7dbacd746cc8b31107eb78de05dda8596896057
crcms <crcms@crcms.cn>
2018-06-15 07:30:56 +0800

    bootstrap app path update

--- public/index.php
+++ public/index.php
@@ -25,6 +25,22 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new \CrCms\Foundation\Application(
+    realpath(__DIR__.'/../')
+);
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -37,7 +53,7 @@ require __DIR__ . '/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+require_once '../vendor/crcms/framework/src/Bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
