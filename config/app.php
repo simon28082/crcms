e0bcc0b57901986e1f5ba220e18c9e8f60cb31fc
crcms <crcms@crcms.cn>
2018-08-17 07:47:44 +0800

    debug

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author simon <crcms@crcms.cn>
+ * @datetime 2018-07-14 06:56
+ * @link http://crcms.cn/
+ * @copyright Copyright &copy; 2018 Rights Reserved CRCMS
+ */
+
+return [
+    'providers' => [
+        \CrCms\Foundation\Client\ClientServiceProvider::class,
+        \CrCms\Foundation\Rpc\RpcServiceProvider::class
+    ]
+];
