be8d3717701fa93c5280a9e2246ed3bcbef49ef6
crcms <crcms@crcms.cn>
2018-10-09 10:53:01 +0800

    micro-service config

--- config/micro-service.php
+++ config/micro-service.php
@@ -1,65 +1,27 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | Service provider that needs to be loaded in a microservice environment
+    |
+    */
+
     'providers' => [
         \CrCms\Foundation\MicroService\Server\RouteServiceProvider::class,
     ],
 
-    'default' => 'consul',
-
-
-
-
-    'connections' => [
-        'consul' => [
-            'register' => [
-                'uri' => 'v1/agent/service/register',
-                'id' => 's10',
-                'name' => 'test_abc',
-                'tags' => [],
-                'enableTagOverride' => false,
-                'service' => [
-                    'address' => 'localhost',
-                    'port' => '8099',
-                ],
-                /*'check' => [
-                    'id' => '',
-                    'name' => '',
-                    'tcp' => 'localhost:8099',
-                    'interval' => 10,
-                    'timeout' => 1,
-                ],*/
-            ],
-            'discovery' => [
-                'uri' => 'v1/catalog/service',
-                'services' => [
-                    'user',
-                    'passport'
-                ],
-            ],
-            'driver' => [
-                'name' => 'restful',
-                'headers' => [
-                    'User-Agent' => 'CRCMS-JSON-RPC PHP Client',
-                    'Content-Type' => 'application/json',
-                    'Accept' => 'application/json',
-                ]
-            ],
-        ],
-    ],
-
-
     /*
     |--------------------------------------------------------------------------
-    | Connection pool selector
+    | Service server key
     |--------------------------------------------------------------------------
     |
-    | Different selectors can be selected to select the connection in the connection pool
-    | RandSelector: Randomly select an available selector
-    | RingSelector: A ring selector to ensure scheduling equalization
-    | ResidentSelector: Always use the same available selector
-    | PopSelector: Swoole coroutines are used, each time an independently generated connection
+    | Microservice interactive authentication key
+    |
     */
 
-    'selector' => \CrCms\Foundation\Rpc\Client\Selectors\RandSelector::class,
+    'secret' => env('SERVICE_SERVER_SECRET', null),
 ];
