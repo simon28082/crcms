87883d562a9918c75431ed4a4919951f05b0fad0
crcms <crcms@crcms.cn>
2018-10-13 16:46:18 +0800

    Structural optimization

--- public/index.php
+++ public/index.php
@@ -27,20 +27,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__ . '/../vendor/crcms/framework/src/Bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
@@ -51,6 +37,4 @@ $app = require_once __DIR__ . '/../vendor/crcms/framework/src/Bootstrap/app.php'
 |
 */
 
-getenv('CRCMS_RUN_MODE') ?: putenv('CRCMS_RUN_MODE=HTTP');
-
-\CrCms\Foundation\Start::run($app, getenv('CRCMS_RUN_MODE'), $_SERVER['argv'] ?? []);
+\CrCms\Foundation\Start::run($_SERVER['argv'] ?? []);
