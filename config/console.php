49ff94f9fa120a586c3f0e1b8f5a0b1379a79a3e
crcms <crcms@crcms.cn>
2018-10-15 20:00:00 +0800

    console.conf

---
+++ config/console.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Define the application's command schedule.
+    |--------------------------------------------------------------------------
+    |
+    | Enter different execution classes
+    | You must implement the interface CrCms\Foundation\Console\ScheduleDispatchContract
+    |
+    | Example
+    | App\Schedules\Clear::class
+    |
+    */
+
+    'schedules' => [
+
+    ],
+];
