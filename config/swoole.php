414e32a5ce578613b73e08e179dbc0e0c41b95ec
crcms <crcms@crcms.cn>
2018-10-16 11:14:57 +0800

    log level

--- config/swoole.php
+++ config/swoole.php
@@ -7,6 +7,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | All swoole server collections
+    | *.settings.log_level: 0 =>DEBUG 1 =>TRACE 2 =>INFO 3 =>NOTICE 4 =>WARNING 5 =>ERROR
     |
     */
 
@@ -17,7 +18,8 @@ return [
             'mode' => defined('SWOOLE_PROCESS') ? SWOOLE_PROCESS : 3,
             'type' => defined('SWOOLE_SOCK_TCP') ? SWOOLE_SOCK_TCP : 1,
             'settings' => [
-                'log_file' => storage_path('logs/micro-service.log')
+                'log_level' => 4,
+                'log_file' => storage_path('logs/micro-service.log'),
             ]
         ],
     ],
